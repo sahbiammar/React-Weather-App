3546723f0b9044199a82e419866e260e803c55cc
sahbiammar <sahbi.ammar.1995@gmail.com>
2020-09-14 02:53:25 +0200

    Updates

---
+++ precache-manifest.2ce281a15abbdb1b6774902466bfd8fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36109adc65cfbbcb001a644586ae1779",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "e4758a09b281958beda4",
+    "url": "/weather/static/css/main.32366c35.chunk.css"
+  },
+  {
+    "revision": "ee2b49276d6623309c72",
+    "url": "/weather/static/js/2.6b26b080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.6b26b080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4758a09b281958beda4",
+    "url": "/weather/static/js/main.5a6e35e4.chunk.js"
+  },
+  {
+    "revision": "9488df2e9af98cdc878f",
+    "url": "/weather/static/js/runtime-main.a223d469.js"
+  }
+]);
